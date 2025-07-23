fc44cadd81dbd8efff3944c379b517749461cdf4
BoniolEllane <74847517+BoniolEllane@users.noreply.github.com>
2025-07-23 10:38:28 +0800

    d

--- src/Web.jsx
+++ src/Web.jsx
@@ -15,8 +15,8 @@ import cat7 from '/src/assets/section6.png';
 
 
 const Web = () => {
-  const soundUrl = '/src/assets/catnormal.mp3';
-  const soundUrl1 = '/src/assets/monstercat.mp3';
+  const soundUrl = '/catnormal.mp3';
+  const soundUrl1 = '/monstercat.mp3';
   const [playbackRate, setPlaybackRate] = React.useState(0.75);
   const [playbackRate1, setPlaybackRate1] = React.useState(0.75);
   const [play] = useSound(soundUrl, {
